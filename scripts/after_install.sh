18242c0a2dc4263506f5a7a3db5c0a74966b93bc
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-09-22 14:23:24 -0400

    updated pipeline scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /home/ec2-user/tetris_clone
+cd /home/ec2-user/tetris_clone_deploy
+cp -arp dist /home/ec2-user/tetris_clone/
+cp -arp dist-backend /home/ec2-user/tetris_clone/
 
 ls -la
 
