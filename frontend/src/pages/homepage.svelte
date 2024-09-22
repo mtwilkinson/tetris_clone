cbcc50447e9b4e9acdedf28076019997a6bc8979
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-09-22 14:37:49 -0400

    test pipeline scripts

--- frontend/src/pages/homepage.svelte
+++ frontend/src/pages/homepage.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="flex flex-col gap-10">
+    <h1>This is a test</h1>
     <a href="/play">
         <button class="bg-secondary w-64 h-16 text-white rounded-2xl text-3xl font-bold hover:bg-secondaryLight
         hover:text-black outline-4 outline outline-black">Play</button>
